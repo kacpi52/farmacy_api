3d952771683e04d88e8f9bb63c2ebb82595c3378
kacper <kacperkrajewski52@gmail.com>
2024-12-11 21:35:18 +0100

    build: tf for deploy infra add [skip ci]

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "prefix for resources in aws"
+  default = "faa"
+}
+
+variable "project" {
+  description = "project name for tagging resources "
+  default = "farmacy-app-api"
+}
+
+variable "contact" {
+  description = "contact email for tagging reosurces"
+  default = "kacdevtest@gmail.com"
+}
