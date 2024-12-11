d51116885a7eb8ba981d224469378e9914b23e38
kacper <kacperkrajewski52@gmail.com>
2024-12-11 16:26:25 +0100

    build: fix compose

--- infra/setup/main.tf
+++ infra/setup/main.tf
@@ -1,29 +1,29 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "5.23.0"
+      source  = "hashicorp/aws"
+      version = "5.23.0"
     }
   }
 
   backend "s3" {
-    bucket = "devops-farmacy-app-tf-state"
-    key = "tf-state-setup"
-    region = "eu-central-1"
-    encrypt = true
+    bucket         = "devops-farmacy-app-tf-state"
+    key            = "tf-state-setup"
+    region         = "eu-central-1"
+    encrypt        = true
     dynamodb_table = "devops-farmacy-app-tf-lock"
   }
 }
 
 provider "aws" {
   region = "eu-central-1"
-#   tags for identify created infra
+  #   tags for identify created infra
   default_tags {
     tags = {
       Environment = terraform.workspace
-      Project = var.project
-      Contact = var.contact
-      ManageBy = "Terraform/setup"
+      Project     = var.project
+      Contact     = var.contact
+      ManageBy    = "Terraform/setup"
     }
   }
 }
--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,19 +1,19 @@
 variable "tf_state_bucket" {
   description = "name of s3 bucket in AWS for storing tf state"
-  default = "devops-farmacy-app-tf-state"
+  default     = "devops-farmacy-app-tf-state"
 }
 
 variable "tf_state_lock_table" {
   description = "name of the dynamoDb table for tf state locking "
-  default = "devops-farmacy-app-tf-lock"
+  default     = "devops-farmacy-app-tf-lock"
 }
 
 variable "project" {
   description = "project name for tagging resources"
-  default = "farmacy-app-api"
+  default     = "farmacy-app-api"
 }
 
 variable "contact" {
   description = "contact name for tagging resources"
-  default = "kacdevtest@gmail.com"
+  default     = "kacdevtest@gmail.com"
 }
