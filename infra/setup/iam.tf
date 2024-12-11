f3bb687532d1e39516c2b7ab3efcc298ed3c6705
kacper <kacperkrajewski52@gmail.com>
2024-12-11 23:59:56 +0100

    build: create iam and policies for cicd user

---
+++ infra/setup/iam.tf
@@ -0,0 +1,50 @@
+# create IAM user and policies for the cicd 
+resource "aws_iam_user" "cd" {
+  name = "farmacy-app-api-cd"
+}
+
+resource "aws_iam_access_key" "cd" {
+  user = aws_iam_user.cd.name
+}
+
+# create permissions for user and policy for s3 and dynamodb access
+# permission to list specified bucket with tf state and get data to create policies
+data "aws_iam_policy_document" "tf_backend" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
+  }
+
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = [
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"]
+  }
+}
+# create policy
+resource "aws_iam_policy" "tf_backend" {
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  description = "allow user to use s3 and dynamodb for tf backend resources"
+  policy      = data.aws_iam_policy_document.tf_backend.json
+}
+
+# attach user to the policy
+resource "aws_iam_user_policy_attachment" "tf_backend" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.tf_backend.arn
+}
