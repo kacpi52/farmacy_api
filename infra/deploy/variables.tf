ba62fb43a037fdadd13ac62026505711fdbb7e73
kacper <kacperkrajewski52@gmail.com>
2024-12-20 09:38:14 +0100

    build: linked role moved to iam

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -37,7 +37,7 @@ variable "django_secret_key" {
 
 variable "dns_zone_name" {
   description = "Domain name"
-  default     = "londonappdev.net"
+  default     = "carfitment.pl"
 }
 
 variable "subdomain" {
